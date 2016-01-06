2cb5373362c647d6e10582c1921713b0a33a7d27
sylcha <chambon.sylvain@gmail.com>
2016-01-06 12:18:52 +0100

    Correction exo 1-1

---
+++ scripts/exo_01-01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+"""
+Correction de l'exercice 1-1 page 6
+
+2016-01-6
+
+"""
+print("A =", (4 * 5 - 10) / 3)
+print("B =", 4 / 3 + 5 * 8 - 3)
+print("C =", (3 ** 2 - 1) * (3 - 5 ** 2))
+print("D =", 2 * (-3) ** 2 - 4 - (-3) + 7)
+print("E =", (4 ** 6 - 12 ** 3) * (1 - 9 ** 4))
+print("F =", (4E21 + 12E23)/8E25)
